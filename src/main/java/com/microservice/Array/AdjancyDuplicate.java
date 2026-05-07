20692f24aea0d23656f207ed3c92225867a1df4d
Mohd Shahil <shahil.sheikh10m@gmail.com>
2026-05-07 16:45:34 +0530

    Practice

---
+++ src/main/java/com/microservice/Array/AdjancyDuplicate.java
@@ -0,0 +1,13 @@
+package com.microservice.Array;
+
+public class AdjancyDuplicate {
+    public static void main(String[] args) {
+        int [] arr={1,2,2,3,4,4,5};
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]==arr[i+1]){
+                System.out.println(arr[i]);
+            }
+        }
+
+    }
+}
